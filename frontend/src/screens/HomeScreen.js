afe34aa7bbb848a9266d501210dd4931eb82ad9b
Shreeansh Gupta <sarojvarshney20@gmail.com>
2020-10-06 10:36:20 +0530

    products now being served from server

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import axios from "axios";
 
 import Product from "../components/Product";
-import products from "../products";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      const res = await axios.get("/api/products");
+
+      setProducts(res.data);
+    };
+
+    fetchAllProducts();
+  });
+
   return (
     <React.Fragment>
       <h1>Our Recent Products</h1>
