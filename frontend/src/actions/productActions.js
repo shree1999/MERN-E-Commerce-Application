b47dcca939953c52b096913ec94772d64ab5e498
Shreeansh Gupta <sarojvarshney20@gmail.com>
2020-10-20 09:28:27 +0530

    added action and reducers for fetching products

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+import {
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productTypes";
+
+const listProducts = () => async dispatch => {
+  try {
+    // first we are setting our appliction that a request is about to be fired
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+
+    const res = await axios.get("/api/products");
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export { listProducts };
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import axios from "axios";
 
+import { listProducts } from "../actions/productActions";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  /*
+    1. useDispatch is used for firing of action that we create and
+    2. useSelector is used for getting the required state given be redux from reducers.
+  */
+
+  const dispatch = useDispatch();
+
+  const productList = useSelector(state => state.productList);
+  const { loading, products, errMessage } = productList;
 
   useEffect(() => {
-    const fetchAllProducts = async () => {
-      const res = await axios.get("/api/products");
-
-      setProducts(res.data);
-    };
-
-    fetchAllProducts();
-  });
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <React.Fragment>
       <h1>Our Recent Products</h1>
       <Row>
-        {products.map(item => (
-          <Col sm={12} md={6} lg={4} key={item._id.toString()}>
-            <Product item={item} />
-          </Col>
-        ))}
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : errMessage ? (
+          <h2>{errMessage}</h2>
+        ) : (
+          products.map(item => (
+            <Col sm={12} md={6} lg={4} key={item._id.toString()}>
+              <Product item={item} />
+            </Col>
+          ))
+        )}
       </Row>
     </React.Fragment>
   );
